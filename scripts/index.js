9c486829e23e9c22c928fab10e674c723451d966
Daniel Ugarov <tyradire@yandex.ru>
2021-04-23 15:47:13 +0300

    second try

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,29 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__name');
+let nameInput = document.querySelector('.popup__field_name');
 let nameHtml = document.querySelector('.profile__title');
-let descriptionInput = document.querySelector('.popup__description');
+let descriptionInput = document.querySelector('.popup__field_description');
 let descriptionHtml = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.popup__form'); 
-
-openPopupButton.addEventListener('click', function () {
-  popup.classList.toggle('popup_opened');
-  nameInput.value = nameHtml.textContent;
-  descriptionInput.value = descriptionHtml.textContent;
-});
-
-closePopupButton.addEventListener('click', function () {
-  popup.classList.toggle('popup_opened');
-});
 
 function formSubmitHandler(event) {
   event.preventDefault();
   nameHtml.textContent = nameInput.value;
-  nameHtml.insertAdjacentElement('beforeend', openPopupButton);
   descriptionHtml.textContent = descriptionInput.value;
   popup.classList.toggle('popup_opened');
 }
 
+function formOpenButton(event) {
+  nameInput.value = nameHtml.textContent;
+  descriptionInput.value = descriptionHtml.textContent;
+  popup.classList.toggle('popup_opened');
+}
+
+function formCloseButton(event) {
+  popup.classList.toggle('popup_opened');
+}
+
 formElement.addEventListener('submit', formSubmitHandler);
+openPopupButton.addEventListener('click', formOpenButton);
+closePopupButton.addEventListener('click', formCloseButton);
