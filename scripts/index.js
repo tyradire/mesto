d9af7e034b9481515f00cba7e7732649b11bc1a5
Daniel Ugarov <tyradire@yandex.ru>
2021-04-24 22:01:41 +0300

    rly first try

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 let openPopupButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopupButton = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__field_name');
+let nameInput = document.querySelector('.popup__input_type_name');
 let nameHtml = document.querySelector('.profile__title');
-let descriptionInput = document.querySelector('.popup__field_description');
+let descriptionInput = document.querySelector('.popup__input_type_description');
 let descriptionHtml = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.popup__form'); 
+
+function formCloseButton() {
+  popup.classList.toggle('popup_opened');
+}
 
 function formSubmitHandler(event) {
   event.preventDefault();
   nameHtml.textContent = nameInput.value;
   descriptionHtml.textContent = descriptionInput.value;
-  popup.classList.toggle('popup_opened');
+  formCloseButton();
 }
 
 function formOpenButton() {
   nameInput.value = nameHtml.textContent;
   descriptionInput.value = descriptionHtml.textContent;
-  popup.classList.toggle('popup_opened');
-}
-
-function formCloseButton() {
-  popup.classList.toggle('popup_opened');
+  formCloseButton();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
